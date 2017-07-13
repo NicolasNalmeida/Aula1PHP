0f7aa8d8603b1529f9e94a9221dcf3e7f9789770
Nicolas <nicolas@kangurunet.com.br>
2017-07-13 15:48:47 -0300

    Finalização aula de Interface, Inicio Classe Abstrata

--- POO/interface/exemplo1.php
+++ POO/interface/exemplo1.php
@@ -3,15 +3,32 @@
 	interface Veiculo
 	{
 		public function acelerar($velocidade);
-		public function freiar($freiar);
+		public function frenar($velocidade);
 		public function trocarMarcha($marcha);
 	}
 
 	class Civic implements Veiculo
 	{
-		
+		public function acelerar($velocidade)
+		{
+			echo "Acelerou ate " . $velocidade . " km/h"; 
+		}
+
+		public function frenar($velocidade)
+		{
+			echo "O veiculo frenou ate " . $velocidade . " km/h";
+		}
+
+		public function trocarMarcha($marcha)
+		{
+			echo "O veiculo engatou a marcha " . $marcha;
+		}
 	}
 
 	$carro = new Civic();
-
+	$carro->trocarMarcha(1);
+	echo "<br>";
+	$carro->frenar(10);
+	echo "<br>";
+	$carro->acelerar(100);
  ?>
